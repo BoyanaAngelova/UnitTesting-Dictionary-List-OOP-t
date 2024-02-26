a0e7af9bd1bbcb34e39080a74c0e0428c27a03e9
BoyanaAngelova <135323451+BoyanaAngelova@users.noreply.github.com>
2024-02-26 16:14:22 +0200

    Add files via upload

---
+++ EmailValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailValidatorTests
+{
+    
+    [TestCase("aba_mail@softuni.bg")]
+    [TestCase("aba.mail@abv.bg")]
+    [TestCase("aba+123@gmail.com")]
+    public void Test_ValidEmails_ReturnsTrue(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+  
+    [TestCase("blabla.blala.com")]
+    [TestCase("bla%bla@%com")]
+    [TestCase(" blaba@mail.domein")]
+    public void Test_InvalidEmails_ReturnsFalse(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+        
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
---
+++ MatchUrlsTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class MatchUrlsTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_ExtractUrls_EmptyText_ReturnsEmptyList()
+    {
+        // Arrange
+        string text = "";
+        List<string> expected = new();
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    // TODO: finish the test
+    [Test]
+    public void Test_ExtractUrls_NoUrlsInText_ReturnsEmptyList()
+    {
+        // Arrange
+        string text = "No valids URLs!!!";
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_ExtractUrls_SingleUrlInText_ReturnsSingleUrl()
+    {
+        // Arrange
+        string text = "Single URL: https://softuni.bg";
+        List<string> expected = new() {"https://softuni.bg"};
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ExtractUrls_MultipleUrlsInText_ReturnsAllUrls()
+    { 
+        // Arrange
+        string text = "Multiple URL: https://www.softuni.bg, https://digital.softuni.bg, https://creative.softuni.bg";
+        List<string> expected = new() { "https://www.softuni.bg", "https://digital.softuni.bg", "https://creative.softuni.bg"};
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ExtractUrls_UrlsInQuotationMarks_ReturnsUrlsInQuotationMarks()
+    {
+        // Arrange
+        string text = "Single URL: \"https://softuni.bg/about\"";
+        List<string> expected = new() { "https://softuni.bg" };
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
