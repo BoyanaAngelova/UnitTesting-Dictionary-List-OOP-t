756c85ce86fcb6c2d8f4eef6adc26d8fa1d0e8b6
BoyanaAngelova <135323451+BoyanaAngelova@users.noreply.github.com>
2024-02-26 16:16:08 +0200

    Add files via upload

---
+++ ArticleTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class ArticleTests
+{
+    private Article _article;
+    [SetUp]
+    public void SetUp()
+    {
+        this._article = new Article();
+    }
+
+
+    [Test]
+    public void Test_AddArticles_ReturnsArticleWithCorrectData()
+    {
+        // Arrange
+        string[] articleData =
+        {
+            "Article Content Author",
+            "Article2 Content2 Authot2",
+            "Article3 Content3 Author3"
+        };
+
+        // Act
+        Article result = this._article.AddArticles(articleData);
+        // Assert
+        Assert.That(result.ArticleList, Has.Count.EqualTo(3));
+        Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article"));
+        Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
+        Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
+    }
+
+    [Test]
+    public void Test_GetArticleList_SortsArticlesByTitle()
+    {
+        // Arrange
+        Article inputArticle = new Article()
+        {
+            ArticleList = new()
+            {
+                new Article()
+                {
+                Author = "Boyana",
+                Content = "Love stories",
+                Title = "I Love"
+                },
+               new Article()
+               {
+                Author = "BoyanaAngelova",
+                Content = "Fantastic stories",
+                Title = "Fantastic world"
+               }
+            }
+        };
+        string expected = $"Fantastic world - Fantastic stories: BoyanaAngelova{Environment.NewLine}I Love - Love stories: Boyana";
+        //Act
+        string result = this._article.GetArticleList(inputArticle, "title");
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
+    {
+        Article inputArticle = new Article();
+        string result = this._article.GetArticleList(inputArticle, "not- criteria");
+
+        Assert.That(result, Is.Empty);
+    }
+    [Test]
+    public void Test_GetArticleList_ReturnsEmptyString_WhenNoArticleInList()
+    {
+        Article inputArticle = new Article();
+        string result = this._article.GetArticleList(inputArticle, "content");
+
+        Assert.That(result, Is.Empty);
+
+    }
+}
---
+++ PersonTests.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace TestApp.UnitTests;
+
+public class PersonTests
+{
+    private Person _person;
+    [SetUp]
+    public void SetUp()
+    { 
+    this._person = new Person();
+    }
+
+    
+    [Test]
+    public void Test_AddPeople_ReturnsListWithUniquePeople()
+    {
+        // Arrange
+        string[] peopleData = { "Alice A001 25", "Bob B002 30", "Alice A001 35" };
+        List<Person> expectedPeopleList = new()
+        {
+            new Person
+            {
+                Name = "Alice",
+                Id = "A001",
+                Age = 35
+
+            },
+            new Person
+            {
+                Name = "Bob",
+                Id = "B002",
+                Age = 30
+            }
+        };
+        // Act
+        List<Person> resultPeopleList = this._person.AddPeople(peopleData);
+
+         //Assert
+        Assert.That(resultPeopleList, Has.Count.EqualTo(2));
+        for (int i = 0; i < resultPeopleList.Count; i++)
+        {
+            Assert.That(resultPeopleList[i].Name, Is.EqualTo(expectedPeopleList[i].Name));
+           Assert.That(resultPeopleList[i].Id, Is.EqualTo(expectedPeopleList[i].Id));
+          Assert.That(resultPeopleList[i].Age, Is.EqualTo(expectedPeopleList[i].Age));
+        }
+    }
+
+    [Test]
+    public void Test_GetByAgeAscending_SortsPeopleByAge()
+    {
+        //Arrange
+        List<Person> people = new()
+       
+        {
+            new Person
+            {
+                Name = "Jhon",
+                Id = "1",
+                Age = 30
+            },
+            new Person
+            {
+                Name = "Marry",
+                Id = "2",
+                Age = 25
+            },
+            new Person
+            {
+                Name = "Ivan",
+                Id = "3",
+                Age = 50
+            }
+        };
+
+        string expected = $"Marry with ID: 2 is 25 years old.{Environment.NewLine}Jhon with ID: 1 is 30 years old.{Environment.NewLine}Ivan with ID: 3 is 50 years old.";
+        //Act
+        string result = this._person.GetByAgeAscending(people);
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    [Test]
+    public void Test_GetByAgeDescending_ReturnsEmptyList_WhenGivenNoData()
+    {
+        //Arrange
+        List<Person> peopleData = new List<Person>();
+        //Act
+        string result = this._person.GetByAgeAscending(peopleData);
+        //Assert
+        Assert.That(result, Is.Empty);
+    }
+
+}
